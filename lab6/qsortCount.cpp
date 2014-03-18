fc5b1c161bc829439e27a65cb15b68b95541fd01
rafadaguiar <rfna@cin.ufpe.br>
2014-03-18 15:14:54 -0700

    q2 and q3 done

--- lab6/qsortCount.cpp
+++ lab6/qsortCount.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-
+#include <math.h>
 using namespace std;
 
 int * x;
+int comps = 0;
+
+int qc(int n){
+	// Q3
+	if(n==0) return 1;
+	return n*0.6 + 2*qc(n/2); // recurrence relation for the quicksort average case
+}
 
 void swap(int & a, int & b) {
 	int tmp = a;
@@ -17,13 +24,20 @@ int randint(int a, int b) {
 }
 
 void quicksort(int a, int b) {
+	// Q2
 	int i, m;
-	if (a >= b) return;
+	if (a >= b){
+		comps++;
+		return;
+	}
 	swap(x[a], x[randint(a,b)]);
 	m = a;
 	for (i = a+1; i <= b; i++) {
-		if (x[i] < x[a])
+		if (x[i] < x[a]){
 			swap(x[++m], x[i]);
+			comps++;
+		}
+
 	}
 	swap(x[a],x[m]);
 	quicksort(a, m-1);
@@ -35,16 +49,26 @@ void quicksort(int a, int b) {
 int main(int argc, char *argv[]) {
 	srand(time(0));
 
-	// change the following code
+	// Average number of comparisons = 5962
+	int repetitions = 100, j;
 	x = new int[NN];
-	for (int i=0; i<NN; ++i) {
-		x[i] = rand() % NN;
+	// x[0]=2;x[1]=-4;x[2]=6;x[3]=1;x[4]=5;x[5]=-3;x[6]=3;x[7]=7;
+	j = repetitions;
+	while(j > 0){	
+		for (int i=0; i<NN; ++i) {
+			x[i] = rand() % NN;
+		}
+		quicksort(0, NN-1);
+		j--;
 	}
-	quicksort(0, NN-1);
-	for (int i=0; i<NN; ++i) {
-		cout << x[i] << " ";
-	}
-	cout << endl;
+	// for (int i=0; i<NN; ++i) {
+	// 	cout << x[i] << " ";
+	// }
+	// cout << endl;
+	cout<<comps/repetitions<<endl;
+	cout<<qc(NN)<<endl;
+	if(comps/repetitions == qc(NN)) cout<<"true"<<endl;
+	else cout<<"false"<<endl;
 
 	delete[] x;
 	return 0;
